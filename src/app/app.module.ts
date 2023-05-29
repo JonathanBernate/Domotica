28280f2a4fce91b6b446855a33a9ad2c1fb4e0a5
Jonathan Bernate <jbernate3@gmail.com>
2023-05-29 13:36:46 -0500

    prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { IonicModule } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage';
-import { firebaseConfig } from './firebase.config';
+
 import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBzsvs5Ljssr9XWDCYB3g4cCSKwxxlo9BI',
-  authDomain: 'gym-app-back.firebaseapp.com',
-  projectId: 'gym-app-back',
-  storageBucket: 'gym-app-back.appspot.com',
-  messagingSenderId: '971612767868',
-  appId: '1:971612767868:web:b7c94ea0dde445fbcbabf9'
-  };
